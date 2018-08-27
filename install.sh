bab54c6cf4c3ec30d1820d6751411e74de910f0f
zhouxudong666 <39668020+zhouxudong666@users.noreply.github.com>
2018-08-27 22:15:54 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,8 +2,10 @@ yum install unzip -y
 cd /www/server/panel/class
 wget https://github.com/zhouxudong666/-/raw/master/bt_crack.zip
 unzip bt_crack.zip
+rm -f bt_crack.zip
 wget -O update.sh http://download.bt.cn/install/update_pro.sh && bash update.sh pro
 wget https://github.com/zhouxudong666/-/raw/master/bt.zip
 unzip -o bt.zip
 rm -f bt.zip
+echo -e "安装完成! 请等待服务器重启后链接宝塔检测是否安装完成"
 reboot
